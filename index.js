73d690c6d5abbfaaed66b9eb68c2206b6e4bb11a
kehewei <kehewei@hellobike.com>
2021-04-20 19:32:15 +0800

    增加菜单

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function choose(names) {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function showLast(chosen) {
+  document.body.innerHTML = `<span style="font-size: 40px;top: calc(50% - 20px);position: absolute;left: calc(50% - 20px);">你今天已经Roll过了<br />吃>>> ${chosen} <<<</span>`
+}
+
+function show(chosen) {
+  document.body.innerHTML = `<span style="font-size: 40px;top: calc(50% - 20px);position: absolute;left: calc(50% - 20px);">吃>>> ${chosen} <<<</span>`
+}
+
+async function choose(names) {
   const result = {}
   let chosen
   let max = 0
   for (var a = 0; a < 100; a++){
     const name = names[Math.floor(Math.random() * names.length)]
+    show(name)
     if(!result[name]){
       result[name] = 1
     }
@@ -13,10 +26,22 @@ function choose(names) {
         chosen = name
       }
     }
+    await sleep(100)
   }
   
-  console.log(result)
   return chosen
 }
 
-document.body.innerHTML = `吃>>> ${choose(["黄焖鸡", "傲椒小面", "KFC", "麦当劳", "乐中乐", "达美乐", "牛排", "羊肉锅", "猪脚饭"])} <<<`
+const now = new Date()
+const key = now.toDateString()
+const lastChosen = localStorage.getItem(key)
+
+if (lastChosen) {
+  showLast(lastChosen)
+} else {
+  choose(["黄焖鸡", "傲椒小面", "KFC", "麦当劳", "乐中乐", "达美乐", "牛排", "羊肉锅", "猪脚饭", "红烧牛肉面", "冒菜", "汉堡王", "烤鱼", "青花椒鱼", "周渝"]).then(chosen => {
+    show(chosen)
+
+    localStorage.setItem(key, chosen)
+  })
+}
